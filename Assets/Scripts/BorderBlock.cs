9e677a6e4771f91e5fe66d3c5d4aabeb546c527d
Sont3k <markus.residence@gmail.com>
2020-05-06 18:32:00 +0300

    Added BorderBlock and CubeEditor

---
+++ Assets/Scripts/BorderBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BorderBlock : MonoBehaviour
+{
+    Vector2Int gridPos;
+    const int gridSize = 1;
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
+        );
+    }
+}
---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    BorderBlock border;
+
+    private void Awake()
+    {
+        border = GetComponent<BorderBlock>();
+    }
+
+    private void Update()
+    {
+        if(border != null)
+        {
+            SnapToGrid();
+        }
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = border.GetGridSize();
+        Vector2 gridPos = border.GetGridPos() * gridSize;
+
+        transform.position = new Vector3(
+            gridPos.x,
+            .5f,
+            gridPos.y
+        );
+    }
+}
