51d4ac43c428694f0946a3da105fdfaa53271ce5
Sont3k <markus.residence@gmail.com>
2020-05-08 15:56:51 +0300

    Replaced movement type for Crawler

--- Assets/Scripts/Enemies/Crawler.cs
+++ Assets/Scripts/Enemies/Crawler.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
-
+// TODO fix crawler moving
 public class Crawler : MonoBehaviour
 {
     PlayerController player;
+    CharacterController characterController;
 
     [SerializeField] float movementSpeed = 200f;
 
     private void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        UpdateEnemyPosition();
+        MoveTowardPlayer();
     }
 
-    private void UpdateEnemyPosition()
+    private void MoveTowardPlayer()
     {
-        float step = movementSpeed * Time.deltaTime;
-        Vector3 currentPlayerPos = player.transform.position;
+        // float step = movementSpeed * Time.deltaTime;
+        // Vector3 currentPlayerPos = player.transform.position;
 
-        if (Vector3.Distance(transform.position, player.transform.position) > 0.001f)
+        // if (Vector3.Distance(transform.position, player.transform.position) > 0.001f)
+        // {
+        //     transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        // }
+
+        Vector3 dir = player.transform.position - transform.position;
+        var movement = dir.normalized * movementSpeed * Time.deltaTime;
+
+        if(movement.magnitude > dir.magnitude)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+            movement = dir;
         }
+
+        characterController.Move(movement);
     }
 }
